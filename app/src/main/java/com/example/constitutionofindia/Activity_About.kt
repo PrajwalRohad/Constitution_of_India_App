20958bb913515ac91cae949483587a8c67846fe7
PrajwalRohad <prajwalrohad@gmail.com>
2023-12-17 19:00:51 +0530

    Added Animations

--- app/src/main/java/com/example/constitutionofindia/Activity_About.kt
+++ app/src/main/java/com/example/constitutionofindia/Activity_About.kt
@@ -2,6 +2,7 @@ package com.example.constitutionofindia
 
 import android.annotation.SuppressLint
 import android.content.SharedPreferences
+import android.graphics.drawable.AnimatedVectorDrawable
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -18,7 +19,9 @@ class Activity_About : AppCompatActivity() {
     val THEME_SELECTED = "theme_selected"
     val NIGHT_MODE = "night_mode"
 
-    lateinit var CoI_SharedPref : SharedPreferences
+    lateinit var CoI_SharedPref: SharedPreferences
+
+    private lateinit var appIconAnimation: AnimatedVectorDrawable
 
     @SuppressLint("SetTextI18n")
     @RequiresApi(Build.VERSION_CODES.P)
@@ -27,7 +30,8 @@ class Activity_About : AppCompatActivity() {
 
         CoI_SharedPref = getSharedPreferences(THEME_PREF, MODE_PRIVATE)
         val themeselected = CoI_SharedPref.getInt(THEME_SELECTED, R.style.ThemeDefault)
-        val nightmode = CoI_SharedPref.getInt(NIGHT_MODE, AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+        val nightmode =
+            CoI_SharedPref.getInt(NIGHT_MODE, AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
         AppCompatDelegate.setDefaultNightMode(nightmode)
         ThemePreference().changeThemeStyle(this, themeselected)
 
@@ -44,24 +48,42 @@ class Activity_About : AppCompatActivity() {
 
         findViewById<ImageView>(R.id.activity_about_ivAppIcon).also {
             it.setImageResource(R.drawable.app_coin_icon_whiteback_figma)
+
+            it.setOnClickListener { view ->
+                it.setImageResource(R.drawable.avd_appicon_192dp)
+                appIconAnimation = it.drawable as AnimatedVectorDrawable
+                appIconAnimation.start()
+
+//                if(appIconAnimation.isRunning){
+//                    it.setImageResource(R.drawable.app_coin_icon_whiteback_figma)
+//                }
+            }
         }
 
 
-        val packageInfo = applicationContext.packageManager.getPackageInfo(applicationContext.packageName,0)
+        val packageInfo =
+            applicationContext.packageManager.getPackageInfo(applicationContext.packageName, 0)
 
         findViewById<TextView>(R.id.activity_about_tvVersionCode).also {
-            it.text = ("Version "+packageInfo.versionName)
+            it.text = ("Version " + packageInfo.versionName)
         }
-
-
-
-
 
 
     }
 
+    override fun onStart() {
+        super.onStart()
+
+//        val iconImage = findViewById<ImageView>(R.id.activity_about_ivAppIcon).apply {
+//
+//        }
+//
+//        iconImage.setOnClickListener({ appIconAnimation.start() })
+    }
+
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if(item.itemId == android.R.id.home){
+        if (item.itemId == android.R.id.home) {
 //            Intent(this, Activity_Main::class.java).also {
 //                startActivity(it)
 //            }
